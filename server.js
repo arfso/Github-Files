86916415dce6276f770a952db71715edf2b602be
arfso <77380329+arfso@users.noreply.github.com>
2021-03-13 21:32:59 +0800

    hello

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
+const weather = require('weather-js');
+weather.find({search: 'Kabacan, PH', degreeType: 'C'}, function(err, result) {
+    if(err) console.log(err);
+   
+    console.log(JSON.stringify(result, null, 2));
+  });
 
-app.set('view engine', 'ejs')
-app.listen(3000)
+  const express = require('express')
+  const app = express()
+   
 
-app.use((req, res, next) => {
-  console.log("Request Made");
-  console.log(`Host:  ${req.hostname}`);
-  console.log(`Path: ${req.path}`);
-  console.log(`Method: ${req.method}`);
-  next();
+  app.set('view engine','ejs');
+
+  app.listen(3000)
+
+  app.use(express.static('public'));
+
+  app.get('/', function (req, res) {
+      weather.find({search: 'Davao, PH', degreeType: 'C'}, function(err, result) {
+    if(err) console.log(err);
+   
+    console.log(JSON.stringify(result, null, 2));
+  
+      res.render('index',{weatherDat: result});
+    //res.sendFile('./views/index.ejs',{root:__dirname});
 });
+  })
 
-app.use(morgan('dev'));
+  app.get('/other', function (req, res) {
+      res.render('other');
+    //res.sendFile('./views/other.ejs',{root:__dirname});
+  })
 
-app.get('/', function (req, res) {
-  res.sendFile('./views/index.ejs', {root:__dirname});
-});
-
-app.get('/other', function (req, res) {
-  res.sendFile('./views/other.ejs', {root:__dirname});
-});
- 
-app.use((req,res)=>{
-  res.status(404).sendFile('.views/404.html', {root:__dirname});
-});
+  app.use((req, res) => {
+      res.render('404');
+      //res.status(404).sendFile('./views/404.ejs',{root:__dirname});
+  })
 
